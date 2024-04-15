7a1d25a85daa08c33999bfee1158c468a675b629
mary free student <masha1207355@gmail.com>
2024-04-15 14:08:01 +0300

    check all

--- src/processing.py
+++ src/processing.py
@@ -1,5 +1,7 @@
 from datetime import datetime
-def filter_by_state(data: list, state="EXECUTED") -> list:
+
+
+def filter_by_state(data: list, state: str = "EXECUTED") -> list:
     function_output = []
     for check in data:
         if check.get("state") == state:
@@ -8,15 +10,21 @@ def filter_by_state(data: list, state="EXECUTED") -> list:
     return function_output
 
 
-def sorted_by_datetime(data: list, order='descending') -> list:
-    sorted_data = sorted(data, key=lambda x: datetime.fromisoformat(x['date']), reverse=(order=='descending'))
+def sorted_by_datetime(data: list, order: str = "descending") -> list:
+    sorted_data = sorted(data, key=lambda x: datetime.fromisoformat(x["date"]), reverse=(order == "descending"))
     return sorted_data
 
 
-print(sorted_by_datetime(data=[{'id': 41428829, 'state': 'EXECUTED', 'date': '2019-07-03T18:35:29.512364'},
-{'id': 939719570, 'state': 'EXECUTED', 'date': '2018-06-30T02:08:58.425572'},
-{'id': 594226727, 'state': 'CANCELED', 'date': '2018-09-12T21:27:25.241689'},
-{'id': 615064591, 'state': 'CANCELED', 'date': '2018-10-14T08:21:33.419441'}]))
+print(
+    sorted_by_datetime(
+        data=[
+            {"id": 41428829, "state": "EXECUTED", "date": "2019-07-03T18:35:29.512364"},
+            {"id": 939719570, "state": "EXECUTED", "date": "2018-06-30T02:08:58.425572"},
+            {"id": 594226727, "state": "CANCELED", "date": "2018-09-12T21:27:25.241689"},
+            {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
+        ]
+    )
+)
 
 print(
     filter_by_state(
