c580e9fca6d6c437cf713ee08304cc2f18265963
mary free student <masha1207355@gmail.com>
2024-04-17 16:15:33 +0300

    add test 2

--- tests/test_processing.py
+++ tests/test_processing.py
@@ -1,31 +1,6 @@
 import pytest
 
-from src import masks, processing, widget
-
-
-def test_masks():
-    """Проверяет функции из модуля masks"""
-    assert masks.mask_card("7000792289606361") == "7000 79** **** 6361"
-    assert masks.mask_account("73654108430135874305") == "**4305"
-
-
-
-@pytest.mark.parametrize(
-    "name_card_and_name_account, expected",
-    [
-        ("Счет 35383033474447895560", "Счет **5560"),
-        ("Maestro 1596837868705199", "Maestro  1596 83** **** 5199"),
-        ("Visa Platinum 8990922113665229", "Visa Platinum  8990 92** **** 5229"),
-    ],
-)
-def test_mask_kard_and_mask_account(name_card_and_name_account, expected):
-    """проверяет первую функцию из widget"""
-    assert widget.mask_kard_and_mask_account(name_card_and_name_account) == expected
-
-
-def test_data_mask():
-    """Проверяет функцию data_mask() без parametrize потому, что принемаем только одно значение"""
-    assert widget.data_mask("2018-07-11T02:26:18.671407") == "11.07.2018"
+from src import processing
 
 
 @pytest.fixture
---
+++ tests/test_widget.py
@@ -0,0 +1,21 @@
+import pytest
+
+from src import widget
+
+
+@pytest.mark.parametrize(
+    "name_card_and_name_account, expected",
+    [
+        ("Счет 35383033474447895560", "Счет **5560"),
+        ("Maestro 1596837868705199", "Maestro  1596 83** **** 5199"),
+        ("Visa Platinum 8990922113665229", "Visa Platinum  8990 92** **** 5229"),
+    ],
+)
+def test_mask_kard_and_mask_account(name_card_and_name_account, expected):
+    """проверяет первую функцию из widget"""
+    assert widget.mask_kard_and_mask_account(name_card_and_name_account) == expected
+
+
+def test_data_mask():
+    """Проверяет функцию data_mask() без parametrize потому, что принемаем только одно значение"""
+    assert widget.data_mask("2018-07-11T02:26:18.671407") == "11.07.2018"
