c7e9deeb5c77bc3e12c27e431b1dc60cf2e4cdc3
mary free student <masha1207355@gmail.com>
2024-05-08 15:03:47 +0300

    refactor utils code

--- src/utils.py
+++ src/utils.py
@@ -1,10 +1,10 @@
 import json
-from typing import List
+from typing import List, Any
 
 import requests
 
 
-def transaction(filename: str) -> List[dict]:
+def list_of_the_transaction(filename: str) -> List[dict]:
     """функция, которая принимает на вход путь до JSON-файла и возвращает список словарей
     с данными о финансовых транзакциях. Если файл пустой, содержит не список или не найден,
      функция возвращает пустой список."""
@@ -22,15 +22,17 @@ def transaction(filename: str) -> List[dict]:
 
 
 filename = "../data/operations.json"
-nwe_list = transaction(filename)
+nwe_list = list_of_the_transaction(filename)
 
 
-def amount_rub(transac: List[dict]) -> float:
-    """функция, которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях"""
-    rub_list = []
-    for amount in transac:
+def transaction_amount_in_rubles(transactions: List[dict]) -> Any:
+    """функция, которая принимает на вход транзакцию и возвращает сумму транзакции (amount) в рублях,
+    возвращает тип float. Если транзакция была в USD или EUR,
+    идет обращение к внешнему API для получения текущего курса валют и конвертации суммы операции в рубли."""
+    for amount in transactions:
         if amount["operationAmount"]["currency"]["code"] == "RUB":
-            rub_list.append(amount["operationAmount"]["amount"])
+            code_rub = amount["operationAmount"]["amount"]
+            return code_rub
 
         elif amount["operationAmount"]["currency"]["code"] != "RUB":
             code = amount["operationAmount"]["currency"]["code"]
@@ -40,31 +42,45 @@ def amount_rub(transac: List[dict]) -> float:
             )
             data = currency_exchange_rate.json()
 
-            with open("../cout.json", "w", encoding="utf-8") as f:
+            with open("../currency.json", "w", encoding="utf-8") as f:
                 json.dump(data, f)
 
-            with open("../cout.json", "r", encoding="utf-8") as f:
+            with open("../currency.json", "r", encoding="utf-8") as f:
                 read_text = json.load(f)
 
                 for currency, rates in read_text["conversion_rates"].items():
                     if currency == "RUB":
                         get_rub = float(rates)
 
-                for rub in transac:
+                for rub in transactions:
                     if rub["operationAmount"]["currency"]["code"] == "USD":
                         rub_to_usd = float(rub["operationAmount"]["amount"]) * get_rub
 
     return rub_to_usd
 
 
+print(nwe_list)
 print(
-    amount_rub(
+    transaction_amount_in_rubles(
         [
             {
                 "id": 542678139,
                 "state": "EXECUTED",
                 "date": "2018-10-14T22:27:25.205631",
                 "operationAmount": {"amount": "90582.51", "currency": {"name": "USD", "code": "USD"}},
+            }
+        ]
+    )
+)
+
+print(
+    transaction_amount_in_rubles(
+        [
+            {
+                "id": 649467725,
+                "state": "EXECUTED",
+                "date": "2018-04-14T19:35:28.978265",
+                "operationAmount": {"amount": "96995.73", "currency": {"name": "руб.", "code": "RUB"}},
             }
         ]
     )
