b02de3d3fb1af961839b62422a5772cc9d877313
mary free student <masha1207355@gmail.com>
2024-05-27 09:46:00 +0300

    add transactions_csv.py and transactions_excel.py

---
+++ src/transactions_csv.py
@@ -0,0 +1,12 @@
+from typing import Any
+
+import pandas as pd
+
+
+def read_csv(filename: str) -> list[Any]:
+    """считывает финансовые операции с файла csv"""
+    csv_file = pd.read_csv(filename, sep=";")
+    return csv_file.to_dict(orient="records")
+
+
+print(read_csv("../data/transactions.csv"))
---
+++ src/transactions_excel.py
@@ -0,0 +1,12 @@
+from typing import Any, Hashable
+
+import pandas as pd
+
+
+def read_excel(path: str) -> list[dict[Hashable, Any]]:
+    """считывает финансовые операции с файла excel"""
+    excel_file = pd.read_excel(path)
+    return excel_file.to_dict(orient="records")
+
+
+print(read_excel("../data/transactions_excel.xlsx"))
