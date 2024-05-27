ce45d80a36d8d2a0a02df9a9240ad0e6204210d9
mary free student <masha1207355@gmail.com>
2024-05-27 17:07:56 +0300

    add test excel

--- tests/test_transactions_csv.py
+++ tests/test_transactions_csv.py
@@ -1,21 +1,27 @@
 import csv
-from unittest.mock import patch, mock_open
+from unittest.mock import mock_open, patch
 
-csv_data = ("id;state;date;amount;currency_name;currency_code;from;to;description\n"
-            "650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol")
+csv_data = (
+    "id;state;date;amount;currency_name;currency_code;from;to;description\n"
+    "650703;EXECUTED;2023-09-05T11:30:32Z;16210;Sol"
+)
 
 mock_csv_file = mock_open(read_data=csv_data)
 
 
 @patch("builtins.open", mock_csv_file)
-def test_read_csv():
+def test_read_csv() -> None:
     rows = []
     with open("../data/transactions.csv") as csv_file:
         for row in csv.reader(csv_file):
             rows.append(row)
 
-    assert rows != [{'id': 650703.0, 'state': 'EXECUTED', 'date': '2023-09-05T11:30:32Z', 'amount': 16210.0,
-                     'currency_name': 'Sol'}]
-
-
-
+    assert rows != [
+        {
+            "id": 650703.0,
+            "state": "EXECUTED",
+            "date": "2023-09-05T11:30:32Z",
+            "amount": 16210.0,
+            "currency_name": "Sol",
+        }
+    ]
