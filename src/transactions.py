03751affec0bf5ccc13befd7200780a55adec68b
mary free student <masha1207355@gmail.com>
2024-06-04 10:57:36 +0300

    add transactions.py and re_transactions.py

---
+++ src/transactions.py
@@ -0,0 +1,155 @@
+from typing import List
+from collections import Counter
+transactions = [{
+    "id": 587085106,
+    "state": "EXECUTED",
+    "date": "2018-03-23T10:45:06.972075",
+    "operationAmount": {
+      "amount": "48223.05",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 41421565395219882431"
+  },
+  {
+    "id": 142264268,
+    "state": "EXECUTED",
+    "date": "2019-04-04T23:20:05.206878",
+    "operationAmount": {
+      "amount": "79114.93",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 19708645243227258542",
+    "to": "Счет 75651667383060284188"
+  },
+  {
+    "id": 873106923,
+    "state": "EXECUTED",
+    "date": "2019-03-23T01:09:46.296404",
+    "operationAmount": {
+      "amount": "43318.34",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Перевод со счета на счет",
+    "from": "Счет 44812258784861134719",
+    "to": "Счет 74489636417521191160"
+  },
+  {
+    "id": 214024827,
+    "state": "EXECUTED",
+    "date": "2018-12-20T16:43:26.929246",
+    "operationAmount": {
+      "amount": "70946.18",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 10848359769870775355",
+    "to": "Счет 21969751544412966366"
+  },
+  {
+    "id": 522357576,
+    "state": "EXECUTED",
+    "date": "2019-07-12T20:41:47.882230",
+    "operationAmount": {
+      "amount": "51463.70",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Счет 48894435694657014368",
+    "to": "Счет 38976430693692818358"
+  },
+  {
+    "id": 895315941,
+    "state": "EXECUTED",
+    "date": "2018-08-19T04:27:37.904916",
+    "operationAmount": {
+      "amount": "56883.54",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод с карты на карту",
+    "from": "Visa Classic 6831982476737658",
+    "to": "Visa Platinum 8990922113665229"
+  },
+  {
+    "id": 596171168,
+    "state": "EXECUTED",
+    "date": "2018-07-11T02:26:18.671407",
+    "operationAmount": {
+      "amount": "79931.03",
+      "currency": {
+        "name": "руб.",
+        "code": "RUB"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 72082042523231456215"
+  },
+  {
+    "id": 716496732,
+    "state": "EXECUTED",
+    "date": "2018-04-04T17:33:34.701093",
+    "operationAmount": {
+      "amount": "40701.91",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Перевод организации",
+    "from": "Visa Gold 5999414228426353",
+    "to": "Счет 72731966109147704472"
+  },
+  {
+    "id": 863064926,
+    "state": "EXECUTED",
+    "date": "2019-12-08T22:46:21.935582",
+    "operationAmount": {
+      "amount": "41096.24",
+      "currency": {
+        "name": "USD",
+        "code": "USD"
+      }
+    },
+    "description": "Открытие вклада",
+    "to": "Счет 90424923579946435907"
+  }]
+
+list_transaction_categories = {"Открытие вклада": "Открытие вклада",
+                               "Перевод организации": "Перевод организации",
+                               "Перевод с карты на карту": "Перевод с карты на карту",
+                               "Перевод со счета на счет": "Перевод со счета на счет"}
+
+def transactions_nwe(transactions: List[dict], list_transaction_categories: dict) -> dict:
+  '''функцию, которая будет принимать список словарей с данными о банковских операциях
+  и словарь категорий операций и возвращать словарь,
+  в котором ключи — это названия категорий, а значения — это количество операций в каждой категории.'''
+  counter = Counter()
+  for transaction in transactions:
+    description = transaction["description"]
+    category = list_transaction_categories.get(description, "Другое")
+    counter[category] += 1
+  return dict(counter)
+
+
+
+print(transactions_nwe(transactions, list_transaction_categories))
+
