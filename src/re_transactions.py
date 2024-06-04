011062aa22a4c080a68ab2e188e02cb6b16d045c
mary free student <masha1207355@gmail.com>
2024-06-04 11:23:01 +0300

    corect fails

--- src/re_transactions.py
+++ src/re_transactions.py
@@ -1,79 +1,57 @@
-from typing import List
 import re
+from typing import List
 
 transactions = [
     {
         "id": 441945886,
         "state": "EXECUTED",
         "date": "2019-08-26T10:50:58.294041",
-        "operationAmount": {
-            "amount": "31957.58",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
-        },
+        "operationAmount": {"amount": "31957.58", "currency": {"name": "руб.", "code": "RUB"}},
         "description": "Перевод организации",
         "from": "Maestro 1596837868705199",
-        "to": "Счет 64686473678894779589"
+        "to": "Счет 64686473678894779589",
     },
     {
         "id": 41428829,
         "state": "EXECUTED",
         "date": "2019-07-03T18:35:29.512364",
-        "operationAmount": {
-            "amount": "8221.37",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
+        "operationAmount": {"amount": "8221.37", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод организации",
         "from": "MasterCard 7158300734726758",
-        "to": "Счет 35383033474447895560"
+        "to": "Счет 35383033474447895560",
     },
     {
         "id": 939719570,
         "state": "EXECUTED",
         "date": "2018-06-30T02:08:58.425572",
-        "operationAmount": {
-            "amount": "9824.07",
-            "currency": {
-                "name": "USD",
-                "code": "USD"
-            }
-        },
+        "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
         "description": "Перевод организации",
         "from": "Счет 75106830613657916952",
-        "to": "Счет 11776614605963066702"
+        "to": "Счет 11776614605963066702",
     },
     {
         "id": 587085106,
         "state": "EXECUTED",
         "date": "2018-03-23T10:45:06.972075",
-        "operationAmount": {
-            "amount": "48223.05",
-            "currency": {
-                "name": "руб.",
-                "code": "RUB"
-            }
-        },
+        "operationAmount": {"amount": "48223.05", "currency": {"name": "руб.", "code": "RUB"}},
         "description": "Открытие вклада",
-        "to": "Счет 41421565395219882431"
-    }]
+        "to": "Счет 41421565395219882431",
+    },
+]
 
-pattern = re.compile(r'Перевод организации')
+
+pattern = "Перевод организации"
+
 
 def transactions_re(transactions: List[dict], pattern: str) -> List[dict]:
-    ''' функциюя, которая будет принимать список словарей с данными о банковских операциях и строку поиска
-    и возвращать список словарей, у которых в описании есть данная строка.'''
+    """функциюя, которая будет принимать список словарей с данными о банковских операциях и строку поиска
+    и возвращать список словарей, у которых в описании есть данная строка."""
     matches = []
+    re_pattern = re.compile(f"{pattern}")
     for transaction in transactions:
-        if pattern.search(transaction["description"]):
+        if re_pattern.search(transaction["description"]):
             matches.append(transaction)
     return matches
 
 
-
 print(transactions_re(transactions, pattern))
-
